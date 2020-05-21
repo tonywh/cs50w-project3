93ef720ceef5af80f9d60c237d09fd1f3f2c689b
Tony Whittam <tony.whittam@gmail.com>
2020-05-21 13:44:34 +0100

    add basic login/logout

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,28 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    if not request.user.is_authenticated:
+        return render(request, "orders/login.html", {"message": None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "orders/user.html", context)
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "orders/login.html", {"message": "Logged out."})
